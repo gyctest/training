014457383a7196da5f9e81f194674b5e1ded2ca3
gouyc <gouyc@suncd.com>
2018-11-12 18:31:43 +0800

    springmvc

---
+++ testWeb/src/main/java/com/demo/servlet/DemoServlet.java
@@ -0,0 +1,25 @@
+package com.demo.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 描述:
+ * <p>
+ *
+ * @author gyc
+ * @version 1.0
+ * @date 2018/11/12 0012
+ */
+@WebServlet("/order")
+public class DemoServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("success..........");
+    }
+}
